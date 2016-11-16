83bf4c0001a4e2fb2374f9249b73ec5449462e47
himynameisruss <himynameisruss@gmail.com>
2016-11-16 18:52:05 -0500

    Updates

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
 function testAlert(){
 	alert('Testing this alert bitch');
 }
+
+function checkForPokemon(){
+	if (!(document.getElementById('ValueCheck').value == "pokemon")){
+		alert('WRONG');
+	} else {
+		alert('CORRECT');
+	}
+}
