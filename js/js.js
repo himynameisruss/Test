a4fa01fca52eaf48019e18be68a5c9d5bf87821e
himynameisruss <himynameisruss@gmail.com>
2016-11-16 20:25:34 -0500

    Nav panel

--- js/js.js
+++ js/js.js
@@ -9,3 +9,12 @@ function checkForPokemon(){
 		alert('CORRECT');
 	}
 }
+
+function openNav() {
+    document.getElementById("SideNav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("SideNav").style.width = "0";
+}
